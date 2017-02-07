c3da3cf1b980878d26661caf56e9ffce47352f41
Giovanni Zambotti <g.zambotti@gmail.com>
2017-02-07 16:57:26 -0500

    add ip location

--- ip_v1.py
+++ ip_v1.py
@@ -10,10 +10,10 @@
 ### Email: g.zambotti@gmail.com
 #################################################################### 
 
-import os, sys, glob, psycopg2
+import os, sys, glob, psycopg2, time, requests, json, geocoder
 
 # connect to postgres 
-conn = psycopg2.connect("dbname=logs user=postgres password=$$$$$$")
+conn = psycopg2.connect("dbname=logs user=postgres password=postgres")
 
 #################################################################
 ### loop all the logs file with the Windows Server
@@ -21,8 +21,8 @@ conn = psycopg2.connect("dbname=logs user=postgres password=$$$$$$")
 
 def loopOriginalFiles(path):      
     for fname in glob.glob(path):
-        print(fname.split("\\")[4])
-        my_file = fname.split("\\")[4]
+        print(fname.split("\\")[5])
+        my_file = fname.split("\\")[5]
         #my_file = open(fname.split("\\")[4])        
         removeLines(my_file)
         importCopyFile()        
@@ -54,7 +54,7 @@ def removeLines(my_file):
     for i, line in enumerate(f):
         if not line.lstrip().startswith('#'):
             #print line
-            output.write(line)
+            output.write(line.replace('"',''))
     output.close()
 
 #################################################################
@@ -81,10 +81,46 @@ def delete_from_tlog():
     sql = "delete from tlog;"
     cur.execute(sql)
     conn.commit()
-    cur.close     
+    cur.close
+
+
+def iptolocation():    
+    cur = conn.cursor()
+    #sql = "select * from tip where lat1 is null"
+    sql = "select * from tip"
+    cur.execute(sql)
+    print "Start Time : %s" % time.ctime()
+    i = -1
+    n = 10000
+    for row in cur:
+        i += 1
+        if i == n:            
+            n += 10000
+            print '.......pause....', n
+            time.sleep(3600)
+        print i, row[0], n
+        #time.sleep(1)        
+        g = geocoder.freegeoip(row[0])
+        #print g.json
+        dIP = 'unknown'
+        dLoc = '0'
+        dCity = 'unknown'
+        dRegion = 'unknown'
+        dCountry = 'unknown'
+        #print g.json.get('lat',defaultip), i, row[0], n
+        ipupdate(g.json.get('lat',dLoc), g.json.get('lng',dLoc), g.json.get('city',dCity), g.json.get('region_code',dRegion), g.json.get('country_code3',dCountry), g.json.get('ip',dIP))
+        
+    conn.close()
+
+def ipupdate(lat, lng, c, r, cn, ip):    
+    cur = conn.cursor()
+    sql = "update tip_up set lat = " + str(lat) + ", lng = " + str(lng) + ", city = '" + c.replace("'", "-") + "', region ='" + r.replace("'", "-") + "', country ='" + cn.replace("'", "-") + "' where cipunique = '" + ip + "'"
+    cur.execute(sql)
+    conn.commit()
+    cur.close    
         
 if __name__ == '__main__':    
-    #loopfiles('N:\inetpub\logs\LogFiles\W3SVC1\*.log')
-    loopOriginalFiles(r'C:\gis\p2017\log\*.log')
-    
+    #loopOriginalFiles('N:\inetpub\logs\LogFiles\W3SVC1\u_ex15*.log')
+    #loopOriginalFiles(r'C:\gis\p2017\log\*.log')
+    iptolocation()
     
