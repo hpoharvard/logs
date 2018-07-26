02be2eb2094e9eb1b9c29e7c3a2bfa93547805c9
Giovanni Zambotti <g.zambotti@gmail.com>
2018-07-26 12:10:01 -0400

    new README and added uniqueFiles, stripExt function

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ PDF file does not exist then a new one will be created. The script uses the img2
 PDF file does not exist then a new one will be created. The script uses the imagemagick (https://www.imagemagick.org/script/index.php) 
 and wand lib (http://docs.wand-py.org/en/0.4.1/guide/write.html)
 
-4. (crawl_adobe.py) Python script to crawl the PIRC Vault folders, identify all tiff files and the correspondent pdf file. If a correspondent PDF file does not exist, a new one will be created using the Adobe Acrobat DC. The PDF will be created using the Adobe Acrobat DC settings. The script uses the Interapplication Communication API (https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/iac_api_reference.pdf) to create the PDF and this python library (https://pypi.org/project/pywin32/) to connect to Adobe Acrobat.
+4. (crawl_adobe.py) Python script to crawl the PIRC Vault folders, identify all tiff files and the correspondent pdf file. If a correspondent PDF file does not exist, a new one will be created using the Adobe Acrobat DC. The PDF will be created using the Adobe Acrobat DC settings. The script uses the Interapplication Communication API (https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/iac_api_reference.pdf) to create the PDF and this python library (https://pypi.org/project/pywin32/, http://nullege.com/codes/search/win32com, https://github.com/mhammond/pywin32) to connect to Adobe Acrobat.
 The script accepts two arguments. The -d argument allow to specify the path, while the –f argument allow to specify name of the output file. Ex: (python -d C:\Temp –f demo)
 
 
--- crawl_adobe.py
+++ crawl_adobe.py
@@ -3,12 +3,10 @@ __author__ = "Giovanni Zambotti"
 __copyright__ = ""
 __credits__ = ["Giovanni Zambotti"]
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "Giovanni Zambotti"
 __email__ = "g.zambotti@gmail.com"
 __status__ = "Production"
-
-
 """
 import sys, os, time, collections, argparse
 from win32com.client.dynamic import Dispatch
@@ -32,68 +30,50 @@ def makePDF(path, filename):
     avdoc.Open(src, src)
     pddoc = avdoc.GetPDDoc()
     pddoc.Save(1, os.path.abspath(path + "\\" + filename + ".pdf")) 
-    #pddoc.Close()
-    #avdoc.close(-1)  
+    pddoc.Close()
+    avdoc.close(-1)  
+
+# create a new list that contains only unique files 
+def uniqueFiles(vlist, dirpath):
+    vlist_noext = []            
+    for item in vlist:                    
+        vlist_noext.append(item.rsplit('.',1)[0])    
+    c  = collections.Counter(vlist_noext)
+    vlist_new = ([n for n in c if c[n]==1])
+    stripExt(vlist_new, vlist, dirpath)
+
+# create a new list a strip out all the extensions             
+def stripExt(vlist_new, vlist, dirpath):
+    vlist_final = []
+    for item in vlist_new:
+        for vitem in vlist:
+            if item == vitem.rsplit('.',1)[0] and (vitem.rsplit('.',1)[1] == 'tif' or vitem.rsplit('.',1)[1] == 'TIF'):        
+                vlist_final.append(item)    
+    vlist_final.sort()
+    
+    if len(vlist_final) == 0:
+        print ("All set at: " + dirpath)                
+    else:
+        print ("Created pdfs @: " + dirpath + "\n")                
+        for item in vlist_final:
+            print (item + ","+ dirpath)
+            createLOG(item + ","+ dirpath + "\n")
+            makePDF(dirpath, item)
 
 # crawl folders
 def crawlFolders(rootpath):    
-    for dirpath, dirnames, filenames in os.walk(rootpath):        
-        #print ("ORIGINAL LIST: " + str(filenames), str(len(filenames)))
+    for dirpath, dirnames, filenames in os.walk(rootpath):                
+        # crawl all folders with more then a single file        
         if len(filenames) > 1:
             vlist = filenames           
-            vlist = list(filter(lambda x: x.split('.')[-1] == 'pdf' or x.split('.')[-1] == 'tif' or x.split('.')[-1] == 'TIF', vlist))            
-            
-            vlist_noext = []            
-            for item in vlist:                    
-                vlist_noext.append(item.rsplit('.',1)[0])                     
-            
-            c  = collections.Counter(vlist_noext)
-            vlist_new = ([n for n in c if c[n]==1])
-                       
-            vlist_final = []
-            for item in vlist_new:
-                for vitem in vlist:
-                    if item == vitem.rsplit('.',1)[0] and (vitem.rsplit('.',1)[1] == 'tif' or vitem.rsplit('.',1)[1] == 'TIF'):        
-                        vlist_final.append(item)    
-            vlist_final.sort()
-                                    
-            if len(vlist_final) == 0:
-                print ("All set at: " + dirpath)                
-            else:
-                print ("Created pdfs @: " + dirpath + "\n")                
-                for item in vlist_final:
-                    print (item + ","+ dirpath)
-                    createLOG(item + ","+ dirpath + "\n")
-                    makePDF(dirpath, item)
-               
+            # create a list of files with pdf, tif, or TIF extensions only 
+            vlist = list(filter(lambda x: x.split('.')[-1] == 'pdf' or x.split('.')[-1] == 'tif' or x.split('.')[-1] == 'TIF', vlist))                        
+            uniqueFiles(vlist, dirpath)               
         else:
             vlist = filenames           
-            vlist = list(filter(lambda x: x.split('.')[-1] == 'pdf' or x.split('.')[-1] == 'tif' or x.split('.')[-1] == 'TIF', vlist))
-                        
-            vlist_noext = []
-            for item in vlist:                    
-            	vlist_noext.append(item.rsplit('.',1)[0])
-            
-            c  = collections.Counter(vlist_noext)
-            vlist_new = ([n for n in c if c[n]==1])
-            
-            vlist_final = []
-            
-            for item in vlist_new:
-                for vitem in vlist:
-                    if item == vitem.rsplit('.',1)[0] and (vitem.rsplit('.',1)[1] == 'tif' or vitem.rsplit('.',1)[1] == 'TIF'):        
-                        vlist_final.append(item)
-            vlist_final.sort()            
-                       
-            if len(vlist_final) == 0:
-                print ("All set at: " + dirpath)                
-            else:
-                print ("Created pdfs @: " + dirpath + "\n")                
-                for item in vlist_final:
-                    print (item + ","+ dirpath)
-                    createLOG(item + ","+ dirpath + "\n")
-                    makePDF(dirpath, item)
-            
+            vlist = list(filter(lambda x: x.split('.')[-1] == 'pdf' or x.split('.')[-1] == 'tif' or x.split('.')[-1] == 'TIF', vlist))                        
+            uniqueFiles(vlist, dirpath)            
+
 if __name__ == '__main__':
     filetime = time.strftime("%Y%m%d")
     f = open(args.f + "_" + filetime + '.txt','w')    
