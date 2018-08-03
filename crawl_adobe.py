6dfba3f4d71ea9e8c851d790eb64be634bfd5127
Giovanni Zambotti <g.zambotti@gmail.com>
2018-08-03 10:16:05 -0400

    added try/except to track tif not converted

--- crawl_adobe.py
+++ crawl_adobe.py
@@ -8,7 +8,7 @@ __maintainer__ = "Giovanni Zambotti"
 __email__ = "g.zambotti@gmail.com"
 __status__ = "Production"
 """
-import sys, os, time, collections, argparse
+import sys, os, time, collections, argparse, csv
 from win32com.client.dynamic import Dispatch
 
 parser = argparse.ArgumentParser(description='Add an input path')
@@ -19,20 +19,29 @@ parser.add_argument('-f',help='Name of the output file',action='store')
 
 args = parser.parse_args()
 
-# create log file
+# create log file to track all the pdf created and their location
 def createLOG(info):    
     f.write(info)    
+
+# create log file to track tif file that could not been created and their location
+def createLOGerror(info):    
+    e.write(info)    
     
 # create a new pdf
 def makePDF(path, filename):    
     src = os.path.abspath(path + "\\" + filename + ".tif")
     avdoc = Dispatch("AcroExch.AVDoc")
-    avdoc.Open(src, src)
-    pddoc = avdoc.GetPDDoc()
-    pddoc.Save(1, os.path.abspath(path + "\\" + filename + ".pdf")) 
-    pddoc.Close()
-    avdoc.close(-1)  
-
+    try:
+        avdoc.Open(src, src)
+        pddoc = avdoc.GetPDDoc()
+        pddoc.Save(1, os.path.abspath(path + "\\" + filename + ".pdf")) 
+        pddoc.Close()
+        del pddoc
+        avdoc.close(-1)  
+        del avdoc
+    except:
+        print ("could not convert pdf")
+        createLOGerror(src + "\n")
 # create a new list that contains only unique files 
 def uniqueFiles(vlist, dirpath):
     vlist_noext = []            
@@ -76,6 +85,8 @@ def crawlFolders(rootpath):
 
 if __name__ == '__main__':
     filetime = time.strftime("%Y%m%d")
-    f = open(args.f + "_" + filetime + '.txt','w')    
+    f = open(args.f + "_" + filetime + '.csv','wb')
+    e = open("errors_" + filetime + '.csv','wb')
     crawlFolders(args.d)
     f.close()
+    e.close
